8bee6d135385e5c89286d70e9620a8d5a06e6f6c
Kotaro Terada <tera@toki.waseda.jp>
2015-08-20 20:55:35 +0900

    Create problem (and answer) viewer.

---
+++ viewer/main.hpp
@@ -0,0 +1,32 @@
+#ifndef __MAIN_HPP__
+#define __MAIN_HPP__
+
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <map>
+#include <vector>
+#include <queue>
+
+#include <assert.h>
+#include <getopt.h>
+
+using namespace std;
+
+struct Point{
+	int x;
+	int y;
+};
+
+#include "../single/box.hpp"
+#include "../single/line.hpp"
+#include "../single/board.hpp"
+
+#include "utils.hpp"
+
+
+void initialize(char* filename);
+
+#endif /* __MAIN_HPP__ */
---
+++ viewer/utils.hpp
@@ -0,0 +1,19 @@
+#ifndef __UTILS_HPP__
+#define __UTILS_HPP__
+
+// ２桁の整数をアルファベットに変換（３５以下）
+char changeIntToChar(int n);
+
+// 問題盤を表示
+void printBoard();
+
+// 現状のラインを表示
+void printLine(int i);
+
+// ラインナンバーから色をマッピング
+string getcolorescape(int n);
+
+// 正解を表示（正解表示は数字２桁）
+void printSolution();
+
+#endif
