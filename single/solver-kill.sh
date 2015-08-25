137dd0ddf3d0b8632630445f4c963ebc2eb1d86b
Kotaro Terada <tera@toki.waseda.jp>
2015-08-25 13:11:51 +0900

    Add killer python (top.py).

--- single/solver-kill.sh
+++ single/solver-kill.sh
@@ -1,10 +1,27 @@
 #!/bin/bash -x
 
+# Kill `solver` process
+
 if [ $OS = "Windows_NT" ]
 then
     PIDS=(`ps aux | grep solver | grep -v grep | awk '{ print $1 }'`)
 else
     PIDS=(`ps aux | grep solver | grep -v grep | awk '{ print $2; }'`)
+fi
+
+for pid in ${PIDS[*]}
+do
+kill -9 ${pid}
+done
+
+
+# Kill `python (top.py)` process
+
+if [ $OS = "Windows_NT" ]
+then
+    PIDS=(`ps aux | grep python | grep -v grep | awk '{ print $1 }'`)
+else
+    PIDS=(`ps aux | grep python | grep -v grep | awk '{ print $2; }'`)
 fi
 
 for pid in ${PIDS[*]}
