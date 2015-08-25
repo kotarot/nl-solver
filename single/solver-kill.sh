4facadea58b2e18cab092708aa511c4210e57e1d
Kotaro Terada <tera@toki.waseda.jp>
2015-08-25 13:05:42 +0900

    Modify kill script for cygwin.

--- single/solver-kill.sh
+++ single/solver-kill.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -x
 
-PIDS=(`ps aux | grep solver | grep -v grep | awk '{ print $2; }'`)
+if [ $OSAAA = "Windows_NT" ]
+then
+    PIDS=(`ps aux | grep solver | grep -v grep | awk '{ print $1 }'`)
+else
+    PIDS=(`ps aux | grep solver | grep -v grep | awk '{ print $2; }'`)
+fi
+
 for pid in ${PIDS[*]}
 do
 kill -9 ${pid}
