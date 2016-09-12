957b7f065885f2c38d6b757834bbd493389d3f86
Kotaro Terada <tera@toki.waseda.jp>
2016-09-12 16:50:46 +0900

    SDK: Delete unused lines

--- single_3d_hls/nlsolver.sdk/nlsolver_test/src/nlsolver_test.c
+++ single_3d_hls/nlsolver.sdk/nlsolver_test/src/nlsolver_test.c
@@ -21,32 +21,6 @@ int main()
     print("\r\n\r\n");
     print("Hello World\r\n");
 
-#if 0
-    int size_x = 10, size_y = 5, size_z = 3;
-    //int line_num = 4;
-    //int via_num = 2;
-
-    // テストデータ (マトリックス形式)
-    int x, y, z;
-    char boardmat[MAX_LAYER][MAX_BOXES][MAX_BOXES];
-    for (z = 0; z < MAX_LAYER; z++)
-        for (y = 0; y < MAX_BOXES; y++)
-            for (x = 0; x < MAX_BOXES; x++)
-                boardmat[z][y][x] = -1;
-    for (z = 0; z < size_z; z++)
-        for (y = 0; y < size_y; y++)
-            for (x = 0; x < size_x; x++)
-                boardmat[z][y][x] = 0;
-    // Line#1 (x=0, y=0, z=0) -- (x=7, y=4, z=0)
-    boardmat[0][0][0] = 1; boardmat[0][4][7] = 1;
-    // Line#2 (x=0, y=4, z=0) -- (x=7, y=0, z=1)
-    boardmat[0][4][0] = 2; boardmat[1][0][7] = 2;
-    // Line#3 (x=1, y=2, z=0) -- (x=2, y=2, z=0)
-    boardmat[0][2][1] = 3; boardmat[0][2][2] = 3;
-    // Via#a (x=4, y=2, z=0-1)
-    boardmat[0][2][4] = 100; boardmat[1][2][4] = 100;
-#endif
-
     XNlsolver nlsolver;
     XNlsolver *p_nlsolver = &nlsolver;
     XNlsolver_Config *p_nlsolver_config = NULL;
