3323c12a53061ed234043c7967a8ce9b670a46a9
Kotaro Terada <tera@toki.waseda.jp>
2016-09-14 14:22:58 +0900

    Create download problems script

---
+++ download_all_q.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+for i in {1..9}; do
+    ../conmgr/client/adccli --URL='http://das-adc.appspot.com/' --output="NL_Q0$i.txt" get-q $i
+done
+for i in {10..30}; do
+    ../conmgr/client/adccli --URL='http://das-adc.appspot.com/' --output="NL_Q$i.txt" get-q $i
+done
+
+chmod 644 NL_Q*.txt
