fc506c4282876410d57c7286727491397da7edd8
Kotaro Terada <tera@toki.waseda.jp>
2016-09-15 13:48:43 +0900

    Update problem num

--- download_all_q.sh
+++ download_all_q.sh
@@ -3,7 +3,7 @@
 for i in {1..9}; do
     ../conmgr/client/adccli --URL='http://das-adc.appspot.com/' --output="NL_Q0$i.txt" get-q $i
 done
-for i in {10..30}; do
+for i in {10..35}; do
     ../conmgr/client/adccli --URL='http://das-adc.appspot.com/' --output="NL_Q$i.txt" get-q $i
 done
 
